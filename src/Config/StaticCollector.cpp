1b02501227310cd930d9624ee6124060b9ae75a5
grayondream <GrayOnDream@outlook.com>
2025-03-14 00:30:19 +0800

    feat: add debug resource file path

--- src/Config/StaticCollector.cpp
+++ src/Config/StaticCollector.cpp
@@ -1,6 +1,10 @@
 #include "StaticCollector.hpp"
 
+#ifndef RESOURCE_DIR
 constexpr const char * kResourceRoot = "res";
+#else
+constexpr const char* kResourceRoot = RESOURCE_DIR;
+#endif//RESOURCE_DIR
 
 namespace StaticCollector {
     std::filesystem::path getResPath(){
