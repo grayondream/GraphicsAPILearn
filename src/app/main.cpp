f996fdab3c820254e48805034fac8f70df803458
grayondream <GrayOnDream@outlook.com>
2025-03-13 22:15:40 +0800

    fix: format comments

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -13,20 +13,20 @@ static inline constexpr int GAME_ENABLE_MSAA = true;
 /*
  * Application List:
  * Base: Draw a empty window
- *  Base:                       clear window's color into one color by OpenGL
- *  Triangle                    draw a colored triangle by OpenGL
- *  Rect                        draw a colored rect by OpenGL
- *  SimpleTexture               read a image into texture and draw it on a rect by OpenGL
- *  Cube                        draw a cube by OpenGL
- *  Camera                      create a virtual camera
- *  SimpleLight_Ambinatioin     Global Illumination
- *  SimpleLight_Diffuse         diffuse light
- *  SimpleLight_Specular        Specular light
- *  SimpleLight_Material        Material
- *  SimpleLight_Map             Light Map
- *  SimpleLight_Source_Direction Direction Light Source
- *  SimpleLight_Source_Point    Point Light Source
- *  SimpleLight_Source_Spot     Spot Light Source
+ *  Base:                           clear window's color into one color by OpenGL
+ *  Triangle                        draw a colored triangle by OpenGL
+ *  Rect                            draw a colored rect by OpenGL
+ *  SimpleTexture                   read a image into texture and draw it on a rect by OpenGL
+ *  Cube                            draw a cube by OpenGL
+ *  Camera                          create a virtual camera
+ *  SimpleLight_Ambinatioin         Global Illumination
+ *  SimpleLight_Diffuse             diffuse light
+ *  SimpleLight_Specular            Specular light
+ *  SimpleLight_Material            Material
+ *  SimpleLight_Map                 Light Map
+ *  SimpleLight_Source_Direction    Direction Light Source
+ *  SimpleLight_Source_Point        Point Light Source
+ *  SimpleLight_Source_Spot         Spot Light Source
  */
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nShowCmd) {
     ConsoleDebugger consoleDebugger{};
