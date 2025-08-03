35bbd111dc0670ddfc7db4a395cbb78274f3b9ce
grayondream <GrayOnDream@outlook.com>
2025-08-03 22:27:23 +0800

    feat: 渲染法线贴图

--- src/app/GL/Light/Advanced/GLNormalMapApp.hpp
+++ src/app/GL/Light/Advanced/GLNormalMapApp.hpp
@@ -16,26 +16,25 @@ public:
 	virtual bool init(const HINSTANCE, const WindowDesc& param) override;
 
 protected:
-	virtual void clearColor();
-	virtual void beginDrawScene();
 	virtual void drawScene(const float dt);
-	virtual void endDrawScene();
 	virtual void onMouseMove(WPARAM btnState, int x, int y);
 	virtual void onMouseScroll(const UINT msg, const WPARAM wParam, const LPARAM lParam);
 	virtual void onKeyBoardEvent(const UINT msg, const WPARAM wParam, const LPARAM lParam) override;
 
 private:
 	void compileShader();
-	
-private:
+	void createTextures();
 
-	float _far = 25.0;
-	float _near = 1.0;
+private:
+	GLProgram _program;
 
 	float _curTime{};
 	Camera _camera{};
 	bool _clicked{};
 	Point2D _lastPos{0.0, 0.0};
-	glm::vec4 _lightColor{1.0, 1.0, 1.0, 1.0};
-	std::shared_ptr<GLImageTexture2D> _texture{};
+	unsigned int _vao{};
+	unsigned int _vbo{};
+	bool _enableNormalMap{};
+	std::shared_ptr<GLImageTexture2D> _brick{};
+	std::shared_ptr<GLImageTexture2D> _brickNormal{};
 };
--- src/app/main.cpp
+++ src/app/main.cpp
@@ -55,7 +55,7 @@ namespace EnumUtil = Utils::Enum;
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nShowCmd) {
     ConsoleDebugger consoleDebugger{};
     
-    auto type = AppType::Shadow_PointLight;
+    auto type = AppType::NormalMap;
     auto api = GraphicsType::GL;
     LOGI("Start Graphics Learn!!!");
     LOGI("Select {} Application, Render App With {} API", EnumUtil::EnumName(type), EnumUtil::EnumName(api));
