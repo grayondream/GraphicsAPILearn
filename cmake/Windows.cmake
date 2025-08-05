a355634227fbb2148cbe5c98fbb662043271fde3
grayondream <GrayOnDream@outlook.com>
2025-08-05 21:34:25 +0800

    fix: add windows.cmake

---
+++ cmake/Windows.cmake
@@ -0,0 +1,27 @@
+message(STATUS "Initalize ${CMAKE_SYSTEM_NAME}")
+if(NOT CMAKE_SYSTEM_NAME STREQUAL "Windows")
+    message(FATAL_ERROR "Unsupported system: ${CMAKE_SYSTEM_NAME}")
+endif()
+
+message(STATUS ${DX_SDK_ROOT} "DirectX SDK ROOT: ${DX_SDK_ROOT}")
+if(NOT DX_SDK_ROOT)
+    message(FATAL_ERROR "The DirectX's sdk directory is empty, please set it with the variable DX_SDK_ROOT")
+endif()
+
+message(STATUS "DirectX SDK ROOT: ${DX_SDK_ROOT}")
+if(NOT WINDOWS_SDK_ROOT)
+    message(FATAL_ERROR "The Windows's sdk directory is empty, please set it with the variable WINDOWS_SDK_ROOT")
+endif()
+
+message(STATUS "Windows SDK ROOT: ${WINDOWS_SDK_ROOT}")
+if(NOT WINDOWS_SDK_ROOT)
+    message(FATAL_ERROR "The Windows's sdk directory is empty, please set it with the variable WINDOWS_SDK_ROOT")
+endif()
+
+include_directories(${WINDOWS_SDK_ROOT})
+include_directories(${WINDOWS_SDK_ROOT}/winrt)
+
+if(ENABLE_DX11 OR ENABLE_DX12)
+    include_directories(${DX_SDK_ROOT}/Include)
+    link_directories(${DX_SDK_ROOT}/Lib/x64)
+endif()
