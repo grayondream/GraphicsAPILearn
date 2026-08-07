b1c6df2989b1cd5d129f0bd1352006a961ae30b8
grayondream <grayondream@outlook.com>
2026-08-07 23:20:27 +0800

    [what] (feat) change directory name

--- cmake/Mac.cmake
+++ cmake/Mac.cmake
@@ -3,6 +3,6 @@ if(NOT CMAKE_SYSTEM_NAME STREQUAL "Darwin")
     return()
 endif()
 
-message(STATUS "Initalize ${CMAKE_SYSTEM_NAME}")
+message(STATUS "Initialize ${CMAKE_SYSTEM_NAME}")
 set(CMAKE_OSX_DEPLOYMENT_TARGET "15.1" CACHE STRING "Minimum macOS deployment target")
-message(STATUS "macOS Deployment Target: ${CMAKE_OSX_DEPLOYMENT_TARGET}")s
+message(STATUS "macOS Deployment Target: ${CMAKE_OSX_DEPLOYMENT_TARGET}")
